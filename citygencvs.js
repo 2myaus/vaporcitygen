1069c71777e154c4321f7e1a9511219e5476999d
Daniil Petrov <waddlespinespig@gmail.com>
2024-05-14 13:33:29 -0400

    add webgl

--- citygencvs.js
+++ citygencvs.js
@@ -1,5 +1,5 @@
-const wid = 1000;
-const hgt = 1000;
+const wid = window.innerWidth;
+const hgt = window.innerHeight;
 
 const canvas = document.createElement("canvas");
 document.body.appendChild(canvas);
@@ -29,9 +29,29 @@ let darkwdmin = 400;
 let darkwdmax = 600;
 
 let bldheightmin = 500;
-let bldheightmax = 4000;
+let bldheightmax = 8000;
 
 let numbldgs = (60/10000)*wid/scale;
+let numstars = 0.1*wid
+
+
+
+
+const skyGradient = ctx.createLinearGradient(0, 0, 0, hgt);
+skyGradient.addColorStop(0, '#282b8e');
+skyGradient.addColorStop(0.8, '#585bae');
+skyGradient.addColorStop(1, 'rgb(255, 23, 95)');
+
+ctx.fillStyle = skyGradient;
+ctx.fillRect(0, 0, wid, hgt);
+
+ctx.fillStyle = "#fff";
+for(let n = 0; n < numstars; n++){
+	let x = Math.floor(Math.random()*(wid-2));
+	let y = Math.floor(Math.random()*(hgt-2));
+
+	ctx.fillRect(x, y, 2, 2);
+}
 
 windowWidMin *= scale;
 windowWidMax *= scale;
@@ -104,30 +124,116 @@ for(let i = 0; i < numbldgs * 0.5; i++){
 
 let p1 = {
 	x:-Math.random()*wid*0.5,
-	y:(Math.random()*hgt+hgt)*0.5
+	y:hgt*0.25+Math.random()*hgt*0.5
 };
-let p2 = {
-	x:Math.random()*wid*0.5+wid,
-	y:(Math.random()*hgt+hgt)*0.5
-};
+if(Math.random() < 0.5) p1.x += wid*1.5;
 
-let t1 = 50; //Period of sin wave propegating from this point
-let t2 = 50;
+let t1 = 100; //Period of sin wave propegating from this point
 
+const c2 = document.createElement("canvas");
+c2.width = wid;
+c2.height = hgt;
+
+document.body.appendChild(c2);
+c2.style = "position:absolute;height:90vh;mix-blend-mode:color;";
+
+
+const gl = c2.getContext("webgl");
+gl.clearColor(1, 1, 1, 1);
+gl.clear(gl.COLOR_BUFFER_BIT);
+const vertexShaderSource = `
+attribute vec2 a_position;
+void main() {
+  gl_Position = vec4(a_position, 0, 1);
+}
+`;
+
+const fragmentShaderSource = `
+precision mediump float;
+uniform vec2 u_resolution;
+uniform vec2 u_pointpos;
+uniform float u_t;
+
+void main() {
+    float t = 100.0;
+
+	vec2 fragCoord = vec2(gl_FragCoord.x, u_resolution.y-gl_FragCoord.y);
+
+    vec2 d = abs(fragCoord.xy-u_pointpos);
+    d.x *= sqrt(fragCoord.y/u_resolution.y);
+    d.x = sqrt(d.x/250.0)*250.0;
+    float len = length(d);
+
+    float b = sin(3.141*len/t) * (4.0*t/(len+4.0*t))+0.5;
+    if((mod((floor(fragCoord.x/4.0)), 2.0)!=mod((floor(fragCoord.y/4.0)),2.0))){
+        b *= b;
+    }
+    b *= fragCoord.y/u_resolution.y;
+	if(b < 0.0){
+		b = 0.0;
+	}
+    gl_FragColor = vec4(1, 0.18, 0.74, b);
+}
+`;
+
+function compileShader(gl, source, type) {
+  const shader = gl.createShader(type);
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+  return shader;
+}
+
+const vertexShader = compileShader(gl, vertexShaderSource, gl.VERTEX_SHADER);
+const fragmentShader = compileShader(gl, fragmentShaderSource, gl.FRAGMENT_SHADER);
+
+const program = gl.createProgram();
+gl.attachShader(program, vertexShader);
+gl.attachShader(program, fragmentShader);
+gl.linkProgram(program);
+gl.useProgram(program);
+
+const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+const resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution");
+const pointUniformLocation = gl.getUniformLocation(program, "u_pointpos");
+const periodUniformLocation = gl.getUniformLocation(program, "u_period");
+
+const positionBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([-1, -1, 1, -1, -1, 1, 1, 1]), gl.STATIC_DRAW);
+gl.enableVertexAttribArray(positionAttributeLocation);
+gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
+
+gl.uniform2f(resolutionUniformLocation, canvas.width, canvas.height);
+gl.uniform2f(pointUniformLocation, p1.x, p1.y);
+gl.uniform1f(periodUniformLocation, t1);
+
+gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+
+//ctx.clearRect(0, 0, wid, hgt);
+
+/*
+const ctx2 = c2.getContext("2d");
 for(let x = 0; x < wid; x++){
 	for(let y = 0; y < hgt; y++){
 		let brightness = 0;
-		let d1 = Math.sqrt((x-p1.x)*(x-p1.x)*0.25+(y-p1.y)*(y-p1.y));
-		let d2 = Math.sqrt((x-p2.x)*(x-p2.x)*0.25+(y-p2.y)*(y-p2.y));
+		let dx = Math.abs(x-p1.x);
+		let dy = Math.abs(y-p1.y);
 
-		brightness += Math.sin(Math.PI*d1/(t1)) * ((4*t1)/(d1+4*t1))+1;
-		brightness += Math.sin(Math.PI*d2/(t2)) * ((4*t2)/(d2+4*t2))+1;
-		brightness *= 0.25;
+		dx *= Math.pow(y/hgt, 0.5);
+
+		dx = Math.pow(dx/250, 0.5)*250;
+
+		let d1 = Math.pow(dx*dx+dy*dy, 0.5);
+
+		brightness = Math.sin(Math.PI*d1/(t1)) * ((4*t1)/(d1+4*t1))+0.5;
+		if((Math.floor(x/4) % 2)^(Math.floor(y/4) % 2)) brightness *= brightness;
+
+		brightness *= 1;
+		brightness *= Math.pow(y/hgt, 1.5);
 		if(brightness<0){brightness=0;}
-		
-		ctx.fillStyle = `rgba(255, 0, 0, ${brightness})`;
-		//ctx.fillStyle = `rgb(${brightness255}, ${brightness255}, ${brightness255})`;
-		ctx.fillRect(x, y, 1, 1);
+
+		ctx2.fillStyle = `rgba(255, 46, 189, ${brightness})`;
+		ctx2.fillRect(x, y, 1, 1);
 	}
 }
-
+*/
